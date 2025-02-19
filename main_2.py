d555518a3309362b7ef2ca8ea78ace7e4c94d55b
Cesar Lopes <cesargl@sebraesp.com.br>
2025-02-19 13:40:29 -0300

    Restaura .gitignore após limpar cache

--- main_2.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-
-servername = "spsvsql39\\metas"
-dbname = "HubDados"
-driver = "ODBC+Driver+17+for+SQL+Server"
-engine = create_engine(f'mssql+pymssql://@{servername}/{dbname}?trusted_connection=yes&driver={driver}')
